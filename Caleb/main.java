5e37ec9dab8b4fef6687584b2cfdc9fd9e0dee17
cjmesserly <cjmesserly@crimon.ua.edu>
2022-10-12 14:44:38 -0500

    fix

--- Caleb/main.java
+++ Caleb/main.java
@@ -1,5 +1,7 @@
 public class HelloWorld() {
 	public static void main(String args[]) {
-		
+		calculator = new FibonacciCalculator();
+		int fib5 = calculator.calculateFibonacci(5);
+		System.out.println("Fib(5) is "+fib5);
 	}
 }
