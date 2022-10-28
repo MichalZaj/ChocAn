9445d2cb366b21746fe046b01a08744abfadda7f
cjmesserly <cjmesserly@crimon.ua.edu>
2022-10-28 10:07:57 -0500

    ignore this gabe

--- Caleb/FibonacciCalculator.java
+++ Caleb/FibonacciCalculator.java
@@ -7,3 +7,12 @@ public class FibonacciCalculator {
 		return calculateFibonacci(n-1) + calculateFibonacci(n-2);
 	}
 }
+
+
+public class HelloWorld() {
+	public static void main(String args[]) {
+		calculator = new FibonacciCalculator();
+		int fib5 = calculator.calculateFibonacci(5);
+		System.out.println("Fib(5) is "+fib5);
+	}
+}
--- Caleb/main.java
+++ Caleb/main.java
@@ -1,3 +1,14 @@
+public class FibonacciCalculator {
+	
+	public int calculateFibonacci(int n) {
+		if(n==0 || n==1) {
+			return n;
+		}
+		return calculateFibonacci(n-1) + calculateFibonacci(n-2);
+	}
+}
+
+
 public class HelloWorld() {
 	public static void main(String args[]) {
 		calculator = new FibonacciCalculator();
