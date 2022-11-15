1133f8fb4388efe6a6e942964ffa51e332c0db26
Brent Christian <bmchristian@crimson.ua.edu>
2022-11-15 13:17:36 +0000

    WeeklyServiceRecord.java created online with Bitbucket

---
+++ proj4/main/main/WeeklyServiceRecord.java
@@ -0,0 +1,16 @@
+package main.main;
+public class WeeklyServiceRecord {
+  //member variables
+  Service* servicesProvided; //im not sure how we can know the size needed for servicesProvided so using a pointer
+  
+ public WeeklyServiceRecord() { // this is something I found online
+     servicesProvided = new servicesProvided[2]
+     }
+     
+ public WeeklyServiceRecord(int size) { // this is something I found online
+     servicesProvided = new servicesProvided[size]
+     }
+  public SetSize(int size) { 
+   servicesProvided = new servicesProvided[size]
+   }
+   }
