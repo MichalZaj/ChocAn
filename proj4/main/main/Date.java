08f5c8270ba5305530fce2691cd8b4251fbbc643
Brent Christian <bmchristian@crimson.ua.edu>
2022-11-15 12:44:48 +0000

    Date.java created online with Bitbucket

---
+++ proj4/main/main/Date.java
@@ -0,0 +1,15 @@
+package main.main;
+
+public class Date {
+  //member variables
+  int day;
+  int month;
+  int year;
+  
+  public Date(int m, int d, int y) { //constructor for date class in month/day/year format
+    month = m;
+    day = d;
+    year = y;
+    }
+      
+  
