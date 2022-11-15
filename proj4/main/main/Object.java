68ff9210094626da409e0f02d684d436433163e0
cjmesserly <cjmesserly@crimon.ua.edu>
2022-11-14 22:29:33 -0600

    Test for thomas

---
+++ proj4/main/main/Object.java
@@ -0,0 +1,11 @@
+package main.main;
+
+public class Object {
+	public Object() {
+		
+	}
+	
+	public void saySo() {
+		System.out.println("SAYSO");
+	}
+}
--- proj4/main/main/term.java
+++ proj4/main/main/term.java
@@ -4,6 +4,9 @@ public class term {
 
 	public static void main(String[] args) {
 		System.out.print("Hi!");
+		
+		Object myObj = new Object();
+		myObj.saySo();
 	}
 
 }
