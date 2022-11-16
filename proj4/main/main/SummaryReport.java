e8ae22a01f6fed8a8a7ea4502e939050fe3a1081
gcgros <gcgros@crimson.ua.edu>
2022-11-16 14:55:36 -0600

    summary report draft

---
+++ proj4/main/main/SummaryReport.java
@@ -0,0 +1,16 @@
+package main.main;
+
+/** Class is a total report of weekly financial transactions.
+
+ * @author Gabe Gros */
+
+public class SummaryReport {
+  Provider[] listOfProvdersToBePaid = null;
+  int[] totalConsultationPerProviderArray = null;
+  float[] feePerProviderArray = null;
+  Provider[] listOfProvidersWhoProvideService = null;
+  int totalNumberOfConsultations = 0;
+  int overallFee = 0;
+}
+
+
