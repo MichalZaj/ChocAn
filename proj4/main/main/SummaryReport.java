bc4d9b6a41cfffb7f402a1ed7c79ec3096e718ee
gcgros <gcgros@crimson.ua.edu>
2022-11-16 15:20:30 -0600

    summary report draft

--- proj4/main/main/SummaryReport.java
+++ proj4/main/main/SummaryReport.java
@@ -5,12 +5,29 @@ package main.main;
  * @author Gabe Gros */
 
 public class SummaryReport {
-  Provider[] listOfProvdersToBePaid = null;
-  int[] totalConsultationPerProviderArray = null;
-  float[] feePerProviderArray = null;
-  Provider[] listOfProvidersWhoProvideService = null;
-  int totalNumberOfConsultations = 0;
-  int overallFee = 0;
+  Provider[] listOfProvdersToBePaid  =  null;
+  int[] totalConsultationPerProviderArray  =  null;
+  float[] feePerProviderArray  =  null;
+  Provider[] listOfProvidersWhoProvideService  =  null;
+  int totalNumberOfConsultations  =  0;
+  int overallFee  =  0;
+
+  /** Class prints the report.
+
+   * @author Gabe Gros */
+  public void printSummary() {
+    for (int i = 0; i < listOfProvdersToBePaid.length; i++) {
+      System.out.println(listOfProvdersToBePaid[i] + "has " + totalConsultationPerProviderArray[i]
+          + " consultations for $" + feePerProviderArray[i]);
+    }
+    System.out.println("All current providers: ");
+    for (int i = 0; i < listOfProvidersWhoProvideService.length; i++) {
+      System.out.println(listOfProvidersWhoProvideService[i]);
+    }
+    System.out.println("This week there were " + totalNumberOfConsultations
+        + " consultations.");
+    System.out.println("These consultations were for a total of $" + overallFee);
+  }
 }
 
 
