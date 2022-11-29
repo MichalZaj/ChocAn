7b4543279ab30a8eee527179b35b6dcbb9459805
taroden <taroden@crimson.ua.edu>
2022-11-29 12:23:35 -0600

    summary correction

--- proj4/main/main/SummaryReport.java
+++ proj4/main/main/SummaryReport.java
@@ -4,34 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 /** Class is a total report of weekly financial transactions.
 
- * @author Gabe Gros */
+* @author Gabe Gros */
 
 public class SummaryReport {
-<<<<<<< HEAD
-  Provider[] listOfProvdersToBePaid  =  null;
-  int[] totalConsultationPerProviderArray  =  null;
-  float[] feePerProviderArray  =  null;
-  Provider[] listOfProvidersWhoProvideService  =  null;
-  int totalNumberOfConsultations  =  0;
-  int overallFee  =  0;
-
-  /** Class prints the report.
-
-   * @author Gabe Gros */
-  public void printSummary() {
-    for (int i = 0; i < listOfProvdersToBePaid.length; i++) {
-      System.out.println(listOfProvdersToBePaid[i] + "has " + totalConsultationPerProviderArray[i]
-          + " consultations for $" + feePerProviderArray[i]);
-    }
-    System.out.println("All current providers: ");
-    for (int i = 0; i < listOfProvidersWhoProvideService.length; i++) {
-      System.out.println(listOfProvidersWhoProvideService[i]);
-    }
-    System.out.println("This week there were " + totalNumberOfConsultations
-        + " consultations.");
-    System.out.println("These consultations were for a total of $" + overallFee);
-  }
-=======
   public List<String> providersToBePaid; 
   public List<Integer> numConsultsPerProvider; 
   public List<Double> feePerProvider;
@@ -90,7 +65,4 @@ public class SummaryReport {
   
   
 
->>>>>>> branch 'master' of https://bitbucket.org/tklocklear/Fall2022Team17.git
 }
-
-
